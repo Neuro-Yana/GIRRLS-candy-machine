785f811d132d1011fc2ad0b301f7310071ab9048
MikeL270 <michaelbraydenlance@gmail.com>
2023-12-11 10:31:36 -0700

    improved commenting in communications.cpp

---
+++ hardware-new/Candy_Machine/Candy_Machine.ino
@@ -0,0 +1,25 @@
+#include "config.h"
+#include "hardware_operations.h" 
+#include "communications.h"
+
+// Events set to PC
+char EVENT_CANDY_DISPENSED[3] = {0x25,0x4d,0x43};
+char EVENT_CANDY_TAKEN[3] = {0x25,0x54,0x52};
+int TimesDispensed = 0;
+
+// -------------------------------------------------------------------------------------------- //
+void setup() {
+  //Determine if the python program is present
+  EstablishConnectionToSoftware ();
+  // set up hardware:
+  SetUpHardware();
+}
+// -------------------------------------------------------------------------------------------- //
+
+void loop() {
+  DetermineCommTypes();
+  WatchBeamBreakers();
+  if (getRunTheMotor()) { 
+    MotorMovePrimaryDispense(MOTOR_ROTATION_PER_DISPENSE);}
+}
+// -------------------------------------------------------------------------------------------- //
